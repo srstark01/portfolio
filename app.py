0a34dca248c36953f6543d88b7db13948df54e2f
srstark01 <srstark01@gmail.com>
2022-07-14 19:49:48 +0000

    git actions test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/") # at the endpoint /
 def hello(): # call method hello
-  return 'Hello World!!!'
+  return 'Hello World!'
 
 
 if __name__ == '__main__': # on running python app.py
